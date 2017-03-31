e5323d3d0f8b5ae80cfb7df707416e65f1359800
Agung Dwi Riansah <agungdwiriansah@gmail.com>
2017-03-31 21:02:40 +0700

    tambah binary dan reverse

---
+++ binary.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var input int
+	_, err := fmt.Scanf("%d", &input)
+	var arr []int
+	if err == nil {
+		//generate
+		for input > 0 {
+			rem := input % 2
+			input = (input - rem) / 2
+			arr = append(arr, rem)
+		}
+		//print
+		for i := len(arr) - 1; i >= 0; i-- {
+			fmt.Print(arr[i])
+		}
+		fmt.Println()
+	}
+
+}
---
+++ reverse.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	input := "Agung Dwi Riansah"
+	split := strings.Split(input, " ")
+
+	for i := 0; i < len(split); i++ {
+		l := len(split[i])
+		if i > 0 {
+			fmt.Print(" ")
+		}
+		for j := 0; j < len(split[i]); j++ {
+			if j == 0 {
+				fmt.Print(strings.ToUpper(string(split[i][l-1-j])))
+			} else if j == len(split[i])-1 {
+				fmt.Print(strings.ToLower(string(split[i][l-1-j])))
+			} else {
+				fmt.Print(string(split[i][l-1-j]))
+			}
+
+		}
+
+	}
+	fmt.Println()
+}
